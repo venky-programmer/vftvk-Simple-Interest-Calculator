04f8042b1b9d53d72216421aba95eac18ec6f601
venky-programmer <venkateshdp333@gmail.com>
2021-11-13 19:15:31 +0530

    Simple Interest Calculator Done

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    n = document.getElementById("years").value;
+
+    yearint = new Date().getFullYear()
+
+
+    document.getElementById("principaltext").innerHTML = p;
+    document.getElementById("interesttext").innerHTML = r;
+    document.getElementById("simpleinteresttext").innerHTML = (p*(r/100)*n);
+    document.getElementById("yeartext").innerHTML = parseInt(yearint) + parseInt(n) + (-1);
 }
+
+document.getElementById("mytext").innerHTML = principalamount;
+
+
+function myFunction() {
+    var number  = document.getElementById("principal").value;
+    document.getElementById("myText").innerHTML = number;
+}
+
 
 function show_value(x)
 {
  document.getElementById("slider_value").innerHTML=x;
 }
-        
